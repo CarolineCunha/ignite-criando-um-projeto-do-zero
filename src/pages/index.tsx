905515f4b31784979bffbb2eec50594d4b558329
CarolineCunha <caroline.cunha.b@hotmail.com>
2021-07-09 11:19:28 -0300

    desafio

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,12 @@ import { FiCalendar, FiUser} from 'react-icons/fi';
 import React from 'react';
 import Header from '../components/Header';
 import { getPrismicClient } from '../services/prismic';
-
+import Prismic from '@prismicio/client';
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+import { useState } from 'react';
+import { format } from 'date-fns';
+import ptBR from 'date-fns/esm/locale/pt-BR/index.js';
 
 interface Post {
   uid?: string;
@@ -27,7 +30,21 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
- export default function Home() {
+ export default function Home({ postsPagination}: HomeProps): JSX.Element {
+   const formattedPost = postsPagination.results.map(post =>{
+     return {
+       ...post,
+       first_publication_date: format(
+         new Date(post.first_publication_date),
+         'dd MMM yyyy',
+         {
+           locale: ptBR,
+         }
+       )
+     }
+   })
+   const [post, setPosts] = useState<Post[]>(formattedPost);
+
   return(
     <>
     <main className={commonStyles.container}>
@@ -59,9 +76,34 @@ interface HomeProps {
   )
  }
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+ export const getStaticProps: GetStaticProps = async () => {
+ const prismic = getPrismicClient();
+const postsResponse = await prismic.query([Prismic.Predicates.at('document.type','posts')
+],
+{
+  pageSize: 1,
+}
+);
 
-//   // TODO
-// };
+const posts = postsResponse.results.map(post=> {
+  return{
+    uid: post.uid,
+    first_publication_date: post.first_publication_date,
+    data: {
+      title: post.data.title,
+      subtitle: post.data.subtitle,
+      author: post.data.author,
+    }
+  }
+});
+const postsPagination = {
+  next_page: postsResponse.next_page,
+  results: posts,
+}
+return {
+  props: {
+    postsPagination,
+  }
+}
+
+ };
